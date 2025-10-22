9948991a88a312c84e3daab754d607e253bef095
Pratyush Prasun <ppratyush1203@gmail.com>
2025-10-22 21:23:59 +0530

    Implementing Some features of Sing in and sign up

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+import InputField from '@/components/forms/InputField'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: "Growth",
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology'
+    },
+    mode: 'onBlur',
+  })
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <div>
+      <h1 className='form-title'>Sign Up & Personalised</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+        <InputField
+          name='fullname'
+          label='Full Name'
+          placeholder='Pratyush'
+          register={register}
+          error={errors.fullName}
+          validation={{ required: 'Fullname is required', minLength: 2 }}
+        />
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@jsmastery.com"
+          register={register}
+          error={errors.email}
+          validation={{ required: 'Email name is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email address is required' }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: 'Password is required', minLength: 8 }}
+        />
+
+        <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+          {isSubmitting ? 'Creating Account' : 'Start Your investing Journey'}
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
